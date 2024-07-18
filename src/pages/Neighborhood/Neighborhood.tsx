74a7068b07aa7e67d88c5a3de289dc6567a4e0c1
08mfp <mohamedfaridpatel@gmail.com>
2024-07-18 23:14:27 +0100

    updated tailwind styling

--- src/pages/Neighborhood/Neighborhood.tsx
+++ src/pages/Neighborhood/Neighborhood.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './Neighborhood.css';
 import MapComponent from '../../components/MapComponent/MapComponent';
 import locations from '../../data/locations.json';
 
@@ -24,44 +23,46 @@ const Neighborhood: React.FC = () => {
     };
 
     return (
-        <div className="neighborhood">
-            <h1>Location</h1>
-            <p>Explore our neighbourhood using the map below:</p>
-            <div className="map-section">
+        <div className="bg-gray-50 dark:bg-gray-800 p-4 md:p-8 min-h-screen">
+            <h1 className="text-5xl md:text-6xl font-bold text-gray-900 dark:text-white mb-8 text-center">Location</h1>
+            <p className="text-xl md:text-3xl text-gray-700 dark:text-gray-300 mb-8 text-center">Explore our neighbourhood using the map below:</p>
+            {/* <div className="mb-8 md:mb-12 border-4 border-gray-600 dark:border-gray-300 p-2 md:p-4 rounded-lg"> */}
+            <div className="mb-8 md:mb-12  p-2 md:p-4 rounded-lg">
                 <MapComponent />
             </div>
-            <div className="distances-section">
-                <h1>Nearby Landmarks</h1>
-                <div className="category-container">
+            <div className="pb-32">
+                <h1 className="text-4xl md:text-5xl font-bold text-gray-900 dark:text-white mb-8 text-center">Nearby Landmarks</h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8">
                     {categories.map(category => (
-                        <div key={category} className="category-section">
-                            <h3 className="category-header">{category}</h3>
-                            <ul className="distance-list">
+                        <div key={category} className="bg-gray-100 dark:bg-gray-700 p-4 md:p-6 rounded-lg shadow-lg">
+                            <h3 className="text-2xl md:text-3xl font-semibold text-gray-900 dark:text-white mb-4 text-center">{category}</h3>
+                            <ul className="space-y-4 md:space-y-6">
                                 {locations
                                     .filter(location => location.category === category)
                                     .sort((a, b) => a['time-from-villa'] - b['time-from-villa'])
                                     .slice(0, visibleCounts[category])
                                     .map((location, index) => (
-                                        <li key={index} className="distance-card">
-                                            <div className="distance-card-header">
+                                        // <li key={index} className="bg-white dark:bg-gray-600 border border-gray-200 dark:border-gray-500 rounded-lg p-2 md:p-4 shadow-md">
+                                        <li key={index} className="bg-white dark:bg-gray-600 border  rounded-lg p-2 md:p-4 shadow-md">
+                                            <div className="flex items-center space-x-2 md:space-x-4 mb-2 md:mb-4">
                                                 <img
                                                     src={location.icon}
                                                     alt={location.category}
-                                                    className="location-icon"
+                                                    className="w-8 h-8 md:w-12 md:h-12"
                                                 />
-                                                <h3>{location.name}</h3>
+                                                <h3 className="text-lg md:text-2xl text-gray-900 dark:text-white">{location.name}</h3>
                                             </div>
-                                            <p className='descr'>{location.description}</p>
-                                            <p className='time'>{location['time-from-villa']} minutes away</p>
+                                            <p className='text-sm md:text-lg text-gray-700 dark:text-gray-300'>{location.description}</p>
+                                            <p className='text-sm md:text-lg text-gray-700 dark:text-gray-300'>{location['time-from-villa']} minutes away</p>
                                         </li>
                                     ))}
                             </ul>
-                            <div className="buttons-container">
+                            <div className="flex justify-between mt-4">
                                 {visibleCounts[category] > 3 && locations.filter(location => location.category === category).length > 2 && (
-                                    <button onClick={() => handleSeeLess(category)} className="see-less-button">See Less</button>
+                                    <button onClick={() => handleSeeLess(category)} className="bg-red-500 text-white text-sm md:text-base px-2 md:px-4 py-1 md:py-2 rounded-lg">See Less</button>
                                 )}
                                 {locations.filter(location => location.category === category).length > visibleCounts[category] && (
-                                    <button onClick={() => handleSeeMore(category)} className="see-more-button">See More</button>
+                                    <button onClick={() => handleSeeMore(category)} className="bg-blue-500 text-white text-sm md:text-base px-2 md:px-4 py-1 md:py-2 rounded-lg">See More</button>
                                 )}
                             </div>
                         </div>
