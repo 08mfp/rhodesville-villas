b6bcffc914c0185bf656cb3bddec25c7945b6ed2
08mfp <mohamedfaridpatel@gmail.com>
2024-07-27 01:08:36 +0100

    SEO optimisation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import apartmentimage from '../../assets/images/apartment.jpeg';
 import apartmentImage from '../../../src/assets/images/apartment.jpeg';
 
 const imageGallery = [
@@ -99,23 +98,23 @@ const Home: React.FC = () => {
         className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat"
       >
         <div
-          className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
+          className="absolute inset-0 bg-gray-900/75 sm:bg-gray-900/75 sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
         ></div>
 
         <div
-          className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+          className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:px-12 lg:flex lg:h-screen lg:items-center lg:text-left"
         >
-          <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-            <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
+          <div className="max-w-xl text-center lg:text-left lg:max-w-full lg:pr-16">
+            <h1 className="text-4xl font-extrabold text-white sm:text-6xl lg:text-7xl">
               Step into a world of luxury and comfort at
               <strong className="block font-extrabold text-rose-500"> Rhodesville Villas </strong>
             </h1>
 
-            <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed">
-            RhodesVille Villas is a luxury residential development located in the heart of Lusaka, Zambia, designed to provide a comfortable and luxurious living experience.
+            <p className="mt-4 max-w-lg text-white sm:text-xl lg:text-2xl">
+              Rhodesville Villas is a luxury residential development located in the heart of Lusaka, designed to provide a comfortable and luxurious living experience.
             </p>
 
-            <div className="mt-8 flex flex-wrap gap-4 text-center">
+            <div className="mt-8 flex flex-wrap gap-4 text-center lg:text-left">
               <a
                 href="#"
                 className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
