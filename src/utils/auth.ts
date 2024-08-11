60461860ff02fae60e69cc66f2d5ea815b8bb997
08mfp <mohamedfaridpatel@gmail.com>
2024-08-11 12:24:18 +0100

    cleaned up code and rearranged file directory

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,18 +1,3 @@
-// export const getAuthHeader = () => {
-//     const username = process.env.REACT_APP_BASIC_AUTH_USERNAME;
-//     const password = process.env.REACT_APP_BASIC_AUTH_PASSWORD;
-  
-//     if (!username || !password) {
-//       throw new Error('Missing BASIC_AUTH_USERNAME or BASIC_AUTH_PASSWORD environment variables');
-//     }
-  
-//     const auth = `${username}:${password}`;
-//     return 'Basic ' + btoa(auth);
-//   };
-  
-
-  // src/utils/auth.ts
-// src/utils/auth.ts
 export const getAuthHeader = () => {
     const username = process.env.REACT_APP_BASIC_AUTH_USERNAME;
     const password = process.env.REACT_APP_BASIC_AUTH_PASSWORD;
